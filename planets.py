eb202675c2c48149d477272590658238d2e0202d
isaacjh0 <ihuang06@calpoly.edu>
2024-04-05 09:24:13 -0700

    final

--- planets.py
+++ planets.py
@@ -1,7 +1,16 @@
 # Given earth weight and planet, returns weight on provided planet
 # If string does not match a planet, raise ValueError
 def weight_on_planets(pounds: float, planet: str) -> float:
-   # write your code here
+   
+   if planet == "Mars":
+      return pounds * 0.38
+   elif planet == "Jupiter":
+      return pounds * 2.34
+   elif planet == "Venus":
+      return pounds * 0.91
+   else:
+      raise ValueError
+
    return 0.0
 
 if __name__ == '__main__':    # pragma: no cover
